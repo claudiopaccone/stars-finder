3400e4b0d76b0c3bcab400eaf616b1b402a061ad
Claudio Paccone <claudio.paccone@facile.it>
2017-12-02 10:14:19 +0100

    Add first version of MVI manifest

---
+++ app/src/main/java/it/personal/claudiopaccone/stardfinder/mvi/MVI.kt
@@ -0,0 +1,56 @@
+package it.personal.claudiopaccone.stardfinder.mvi
+
+import io.reactivex.disposables.Disposable
+import io.reactivex.subjects.BehaviorSubject
+import java.lang.ref.WeakReference
+
+interface View
+interface Action
+interface ViewState
+typealias Reducer<A, VS> = (A, VS) -> VS
+
+/* VIEW */
+
+interface MVIView<in VS> : View {
+    fun render(viewState: VS)
+}
+
+
+/* PRESENTER */
+
+interface Presenter<in V : View> {
+    fun onAttach(view: V)
+    fun onDetach()
+    fun bind(view: V)
+}
+
+abstract class MVIPresenter<in V : MVIView<VS>, VS : ViewState> : Presenter<V> {
+
+    private var viewReference: WeakReference<V>? = null
+
+    abstract var currentState: VS
+    protected val viewStatePublishSubject = BehaviorSubject.create<VS>()
+    private lateinit var viewStateCompositeDisposable: Disposable
+
+    override fun onAttach(view: V) {
+        viewReference = WeakReference(view)
+
+        viewStateCompositeDisposable = viewStatePublishSubject.subscribe {
+            currentState = it
+            view.render(it)
+        }
+    }
+
+
+    override fun onDetach() {
+        viewReference?.clear()
+        viewReference = null
+
+        viewStateCompositeDisposable.dispose()
+    }
+
+}
+
+inline fun <VS, reified VA : Action> VA.reduceViewState(previousState: VS, reducer: Reducer<VA, VS>): VS {
+    return reducer(this, previousState)
+}
