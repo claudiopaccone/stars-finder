bfebfa55e631752f7d466c0541fa999d096d477f
Claudio Paccone <claudio.paccone@facile.it>
2017-12-02 10:15:46 +0100

    Add PresenterActivity and PresenterLoader to manage presenter persistence

---
+++ app/src/main/java/it/personal/claudiopaccone/stardfinder/common/PresenterActivity.kt
@@ -0,0 +1,47 @@
+package it.personal.claudiopaccone.stardfinder.common
+
+import android.os.Bundle
+import android.support.v4.app.LoaderManager
+import android.support.v4.content.Loader
+import android.support.v7.app.AppCompatActivity
+import it.personal.claudiopaccone.stardfinder.mvi.Presenter
+import it.personal.claudiopaccone.stardfinder.mvi.View
+
+abstract class PresenterActivity<P : Presenter<V>, V : View> : AppCompatActivity() {
+
+    private val LOADER_ID = 101
+    protected var presenter: P? = null
+
+    abstract val presenterGenerator: () -> P
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        supportLoaderManager.initLoader(LOADER_ID, null, object : LoaderManager.LoaderCallbacks<P> {
+
+            override fun onCreateLoader(id: Int, args: Bundle?): Loader<P> {
+                return PresenterLoader(this@PresenterActivity, presenterGenerator)
+            }
+
+            override fun onLoadFinished(loader: Loader<P>, data: P) {
+                presenter = data
+            }
+
+            override fun onLoaderReset(loader: Loader<P>) {
+                presenter = null
+            }
+
+        })
+    }
+
+    override fun onStart() {
+        super.onStart()
+        presenter?.onAttach(this as V)
+        presenter?.bind(this as V)
+    }
+
+    override fun onStop() {
+        presenter?.onDetach()
+        super.onStop()
+    }
+}
---
+++ app/src/main/java/it/personal/claudiopaccone/stardfinder/common/PresenterLoader.kt
@@ -0,0 +1,38 @@
+package it.personal.claudiopaccone.stardfinder.common
+
+import android.content.Context
+import android.support.v4.content.Loader
+import it.personal.claudiopaccone.stardfinder.mvi.Presenter
+import it.personal.claudiopaccone.stardfinder.mvi.View
+
+class PresenterLoader<P : Presenter<V>, V : View>(
+        context: Context,
+        private val presenterGenerator: () -> P
+) : Loader<P>(context) {
+
+    private var presenter: P? = null
+
+    override fun onStartLoading() {
+
+        // If we already own an instance, simply deliver it.
+        if (presenter != null) {
+            deliverResult(presenter)
+            return
+        }
+        // Otherwise, force a load
+        forceLoad()
+    }
+
+    override fun onForceLoad() {
+        // Create the Presenter using the Factory
+        presenter = presenterGenerator.invoke()
+
+        // Deliver the result
+        deliverResult(presenter)
+    }
+
+    override fun onReset() {
+        presenter = null
+    }
+
+}
